b4789b556a24935688e0e3a14a5f940d4be73d30
JasonYoo1 <jyoojs@gmail.com>
2019-08-19 14:18:27 -0700

    initial commit

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+import React from 'react';
+import { render } from 'react-test-renderer';
+import Display from './Display';
+
+describe ('<Display />', ()=>{
+    it('should display open and unlocked', ()=>{
+        const {getText,queryByText} = render(<Display/>)
+        expect (queryByText(/open/i)).toBeTruthy();
+        expect (queryByText(/unlocked/i)).toBeTruthy();
+    })
+})
