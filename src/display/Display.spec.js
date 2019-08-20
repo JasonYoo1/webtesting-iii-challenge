10e22e7f5c93b9e4c10730fa5b14b24b4218bb57
JasonYoo1 <jyoojs@gmail.com>
2019-08-19 18:35:27 -0700

    green led test open passed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -14,15 +14,7 @@ describe("<Display />", () => {
         expect(tree.toJSON()).toMatchSnapshot();
 
     })
-    it('red-led test', () => {
-        const component = render(<Display closed={true} locked={true}/>)
-        const display = component.getByText('Locked')
-        expect(display).toHaveClass('red-led')
-    })
     
-})
-
-describe ('<Display />', ()=>{
     it('should display open and unlocked', ()=>{
         const {getByText} = render(<Display/>)
         expect(getByText('Unlocked'));
@@ -32,6 +24,16 @@ describe ('<Display />', ()=>{
         const {getByText} = render(<Display locked ={true} closed={true}/>);
         expect(getByText('Locked'));
         expect(getByText('Closed'));
+    })
+    it('red-led test locked', () => {
+        const component = render(<Display closed={true} locked={true}/>)
+        const display = component.getByText('Locked')
+        expect(display).toHaveClass('red-led')
+    })
+    it('green-led test open', () => {
+        const component = render(<Display closed={false} locked={false}/>)
+        const displayopen = component.getByText('Open')
+        expect(displayopen).toHaveClass('green-led')
     })
 })
 
