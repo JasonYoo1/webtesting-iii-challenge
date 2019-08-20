e0d8d22efac3baa67ddac672ed94ab8bac6b93e6
JasonYoo1 <jyoojs@gmail.com>
2019-08-19 17:45:07 -0700

    Controls spec btn toggle passed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,25 +1,22 @@
-// // Test away!
+// Test away!
 
-// import React from 'react';
-// import renderer from 'react-test-renderer'
-// import { render } from '@testing-library/react'
+import React from 'react';
+import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react'
+import Controls from './Controls'
 
-// import Controls from './Controls'
 
-
-// describe('<Controls />', () => {
-//     it("matches snapshot" , () => {
-//         const tree = renderer.create(<Controls/>)  
-//         expect(tree.toJSON()).toMatchSnapshot();
-//       });
-
-//     it('provides buttons for toggling', ()=>{
-//       const { getByTestId } = render (<Controls/>);
-//       const openCloseBtn = getByTestId('control-open-close');
-//       const lockUnlockBtn = getByTestId('control-lock-unlock')
-
-//       expect(openCloseBtn).toBe(true);
-//       expect(lockUnlockBtn).toBe(true)
-//     })
-// })
-    
+describe('<Controls />', () => {
+//snapshot 
+    it("matches snapshot" , () => {
+        const tree = renderer.create(<Controls/>)  
+        expect(tree.toJSON()).toMatchSnapshot();
+      });
+      //fulfills the req 
+      //check buttons to toggle the closed and locked states.
+      it('toggle closed and locked', ()=>{
+        const { getByText } = render(<Controls/>)
+        getByText(/close gate/i)
+        getByText(/lock gate/i)
+    })
+})
