b2874922550cdd25414a5612cc080fe122cc33c4
JasonYoo1 <jyoojs@gmail.com>
2019-08-19 17:37:18 -0700

    made multiple tests for each state with each passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 // Test away
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 
 import Dashboard, { asyncFunc } from './Dashboard'
@@ -17,7 +17,36 @@ describe("<Dashboard />", ()=>{
         expect(getByText('Close Gate'));
         
     })
-})
+    it('checks buttons if it change state', () => {
+        const { getByText } = render(<Dashboard />);
+  
+        const closeButton = getByText("Close Gate")
+        fireEvent.click(closeButton);
+        expect(getByText('Unlocked'));
+        expect(getByText('Closed'));
+        expect(getByText('Lock Gate'));
+        expect(getByText('Open Gate'));
 
+        const lockButton = getByText("Lock Gate")
+        fireEvent.click(lockButton)
+        expect(getByText('Locked'));
+        expect(getByText('Closed'));
+        expect(getByText('Unlock Gate'));
+        expect(getByText('Open Gate'));
 
-//unit test (tests a function)
+        const unlockButton = getByText("Unlock Gate")
+        fireEvent.click(unlockButton)
+        expect(getByText('Unlocked'));
+        expect(getByText('Closed'));
+        expect(getByText('Lock Gate'));
+        expect(getByText('Open Gate'));
+
+        const openButton = getByText("Open Gate")
+        fireEvent.click(openButton)
+        expect(getByText('Unlocked'));
+        expect(getByText('Open'));
+        expect(getByText('Lock Gate'));
+        expect(getByText('Close Gate'));
+
+    })
+});
